096666b706ade2fb93bb60c31d83bbd3b6c67092
LunaFox <fox@trans.fish>
2025-07-04 13:40:31 -0300

    chore: Remove temp veil compat

--- src/main/java/dev/hephaestus/glowcase/client/GlowcaseClient.java
+++ src/main/java/dev/hephaestus/glowcase/client/GlowcaseClient.java
@@ -57,8 +57,6 @@ public class GlowcaseClient implements ClientModInitializer {
 	public static final Boolean EMI_LOADED = FabricLoader.getInstance().isModLoaded("emi");
 	public static final ScreenImageCache screenImageCache = new ScreenImageCache();
 	public static final Identifier PROVIDER_CROSSHAIR_TEXTURE = Glowcase.id("hud/provider_crosshair");
-	// Use a stack so it can be more freely used if needed in more places
-	public static final Stack<Void> PREVENT_VEIL_DYNAMIC_BUFFER = new Stack<>();
 
 	private double accScroll = 0;
 
